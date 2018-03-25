31b733d9fb0c1066055eadfd9ba810e0d1fd5a13
Robert Conde <elconde@alum.mit.edu>
2018-03-25 10:56:25 -0400

    Bump version: 1.1.0 → 1.1.1

--- flickr/__init__.py
+++ flickr/__init__.py
@@ -5,4 +5,4 @@ from .flickr_Authenticate import flickr_session, REST_URL
 from .flickr_Upload import upload_photos
 from .flickr_Photosets import photsets_get_list, photosets_add_photo
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
